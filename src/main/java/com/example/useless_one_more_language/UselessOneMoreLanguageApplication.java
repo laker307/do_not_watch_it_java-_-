f6cdc4d1415fff98b9ebc033150e9a22d861e58a
laker307 <laker307@yandex.ru>
2023-12-16 15:21:44 +0300

    Init

---
+++ src/main/java/com/example/useless_one_more_language/UselessOneMoreLanguageApplication.java
@@ -0,0 +1,13 @@
+package com.example.useless_one_more_language;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UselessOneMoreLanguageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UselessOneMoreLanguageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/useless_one_more_language/UselessOneMoreLanguageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.useless_one_more_language;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UselessOneMoreLanguageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
